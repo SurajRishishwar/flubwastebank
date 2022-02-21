841bd8d9447c933113dfd5d039170fbc82abfb2f
Suraj Sharma <75289779+SurajRishishwar@users.noreply.github.com>
2022-02-21 12:17:35 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('');
+mongoose.connect('mongodb+srv://alpit:AlpitNamdev@cluster0.oueuw.mongodb.net/rrrrData?retryWrites=true&w=majority');
 
 const db =mongoose.connection;
 db.on('error ',console.error.bind(console,"Error"));
